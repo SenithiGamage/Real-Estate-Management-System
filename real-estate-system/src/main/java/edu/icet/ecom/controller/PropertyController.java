fde247084861ef4a2d1a5513d0310cceb94b5f22
SenithiGamage <gamagesenithi25@gmail.com>
2026-05-02 08:43:06 +0530

    Initial setup: Spring Boot backend and Angular frontend initialized

---
+++ real-estate-system/src/main/java/edu/icet/ecom/controller/PropertyController.java
@@ -0,0 +1,12 @@
+package edu.icet.ecom.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/properties")
+@CrossOrigin(origins = "http://localhost:4200")
+
+public class PropertyController {
+}
