1315d1422a0f501561504968c5428912f85749dd
SenithiGamage <gamagesenithi25@gmail.com>
2026-05-03 14:17:30 +0530

    add the login page

---
+++ real-estate-system/real-estate-frontend/src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+
+  onLogin() {
+    alert('Login functionality will be added with JWT later!');
+  }
+}
