4b38d41dfc8053207e1566dd2929b03bbbdf6c28
SenithiGamage <gamagesenithi25@gmail.com>
2026-05-03 14:29:16 +0530

    add property detail component

--- real-estate-system/real-estate-frontend/src/app/app.routes.ts
+++ real-estate-system/real-estate-frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { PropertyListComponent } from './property-list/property-list.component';
+import { PropertyDetailComponent } from './property-detail/property-detail.component';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {PropertyListComponent} from './property-list/property-list.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'properties', component: PropertyListComponent },
+  { path: 'properties/:id', component: PropertyDetailComponent },
   { path: '**', redirectTo: '' }
 ];
